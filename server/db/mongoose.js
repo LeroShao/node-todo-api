c6317fe052a35b99de7af7a3059a8a5c114734d0
LeroShao <ls878@cornell.edu>
2019-01-25 10:19:47 -0800

    change position of URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp' || process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
