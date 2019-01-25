8eb25fe0d6d2534f36344a6edc12341cb8f0c56d
LeroShao <ls878@cornell.edu>
2019-01-25 15:11:54 -0800

    Setup seperate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = Promise;
-mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true });
-// mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 module.exports = {mongoose};
